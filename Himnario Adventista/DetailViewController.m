6f01427177f77b44c8b58bca05a5628bd3c7fe6e
Daniel Scholtus <sdeonline@gmail.com>
2013-07-25 21:49:22 -0300

    Initial commit

--- Himnario Adventista/DetailViewController.m
+++ Himnario Adventista/DetailViewController.m
@@ -34,9 +34,9 @@
 - (void)configureView
 {
     // Update the user interface for the detail item.
-
     if (self.detailItem) {
-        self.detailDescriptionLabel.text = [self.detailItem description];
+        self.detailTitleLabel.title = self.detailItem[0];
+        self.detailDescriptionLabel.text = self.detailItem[1];
     }
 }
 
--- Himnario Adventista/MasterViewController.m
+++ Himnario Adventista/MasterViewController.m
@@ -12,10 +12,12 @@
 
 @interface MasterViewController () {
     NSMutableArray *_objects;
+    NSArray *_content;
 }
 @end
 
-@implementation MasterViewController
+@implementation MasterViewController {
+}
 
 - (void)awakeFromNib
 {
@@ -29,11 +31,25 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    // Copy array into objects array...
+    if (!_objects) {
+        
+        _objects = [@[
+                    @"La Palabra de Dios",
+                    @"Dios Padre",
+                    @"La Deidad"] mutableCopy];
+        
+        _content = @[
+                    @"La Palabra de Dios es la posta",
+                    @"Dios Padre es el Rey Soberano",
+                    @"La Deidad son tres en uno"];
+    }
+    
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
+    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+    
 	// Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.leftBarButtonItem = self.editButtonItem;
-
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
     self.detailViewController = (DetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
 }
 
@@ -41,16 +57,6 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-- (void)insertNewObject:(id)sender
-{
-    if (!_objects) {
-        _objects = [[NSMutableArray alloc] init];
-    }
-    [_objects insertObject:[NSDate date] atIndex:0];
-    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
-    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
 }
 
 #pragma mark - Table View
@@ -74,22 +80,6 @@
     return cell;
 }
 
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        [_objects removeObjectAtIndex:indexPath.row];
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-    }
-}
-
 /*
 // Override to support rearranging the table view.
 - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
@@ -109,7 +99,7 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
-        NSDate *object = _objects[indexPath.row];
+        NSArray *object = @[_objects[indexPath.row],_content[indexPath.row]];
         self.detailViewController.detailItem = object;
     }
 }
@@ -118,7 +108,7 @@
 {
     if ([[segue identifier] isEqualToString:@"showDetail"]) {
         NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-        NSDate *object = _objects[indexPath.row];
+        NSArray *object = @[_objects[indexPath.row],_content[indexPath.row]];
         [[segue destinationViewController] setDetailItem:object];
     }
 }
